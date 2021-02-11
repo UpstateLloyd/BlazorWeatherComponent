380f9768c4b715ee28501b5ea782150535e2a726
Lloyd Hotchkiss <upstatelloyd@gmail.com>
2021-02-10 20:09:35 -0500

    Added WeatherModel with list of available icons

---
+++ Models/WeatherModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherDefinitions
+    {
+        string[] WeatherClass = (sunny, cloudy, cloudy-gusts, cloudy-windy, fog, hail, haze,
+        lightning, rain, rain-mix, rain-wind, showers, sleet, sleet-storm, snow, snow-thunderstorm
+        snow-wind, sprinkle, storm-showers, sunny-overcast, thunderstorm, windy, hot, light-wind,
+        clear, partly-cloudy)
+    } 
+}
--- obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,84 +13,84 @@ namespace WeatherAPI.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 1 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 2 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 3 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 4 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 5 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 6 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 7 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 8 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 9 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using WeatherAPI;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\lhotchkiss\source\repos\WeatherAPI\_Imports.razor"
+#line 10 "/home/upstatelloyd/Projects/BlazorWeatherComponent/_Imports.razor"
 using WeatherAPI.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\lhotchkiss\source\repos\WeatherAPI\Pages\FetchData.razor"
+#line 2 "/home/upstatelloyd/Projects/BlazorWeatherComponent/Pages/FetchData.razor"
 using WeatherAPI.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\lhotchkiss\source\repos\WeatherAPI\Pages\FetchData.razor"
+#line 3 "/home/upstatelloyd/Projects/BlazorWeatherComponent/Pages/FetchData.razor"
 using WeatherAPI.Data;
 
 #line default
@@ -105,7 +105,7 @@ using WeatherAPI.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 34 "C:\Users\lhotchkiss\source\repos\WeatherAPI\Pages\FetchData.razor"
+#line 34 "/home/upstatelloyd/Projects/BlazorWeatherComponent/Pages/FetchData.razor"
        
 
     ForecastModel forecast = new ForecastModel();
