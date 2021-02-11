4b5cbe668514f7dfc456d9d157b51ffdbd038761
Lloyd <UpstateLloyd@gmail.com>
2021-02-11 10:33:06 -0500

    Prep css to NWS dictionary

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -8,9 +8,14 @@ namespace WeatherAPI.Models
 {
     public class WeatherDefinitions
     {
-        public string[] WeatherArray = new string[26] {"sunny", "cloudy", "cloudy-gusts", "cloudy-windy", "fog", "hail", 
-        "haze", "lightning", "rain", "rain-mix", "rain-wind", "showers", "sleet", "sleet-storm", "snow", "snow-thunderstorm",
-        "snow-wind", "sprinkle", "storm-showers", "sunny-overcast", "thunderstorm", "windy", "hot", "light-wind",
-        "clear", "partly-cloudy" };
+        public Dictionary<string, string> WeatherArray = new Dictionary<string, string>() 
+        {
+            { "Fair","sunny" } {"Mostly Cloudy","cloudy" }, {,"cloudy-gusts" }, {,"cloudy-windy" }, {,"fog" }, {,"hail" },
+            { "Haze","haze" }, {,"lightning" }, {,"rain" }, {,"rain-mix" }, {,"rain-wind" }, {,"showers" }, {,"sleet" }, {,"sleet-storm" },{,"snow" },
+            {,"snow-thunderstorm" }, {,"snow-wind" }, {,"sprinkle" }, {,"storm-showers" }, {,"sunny-overcast" }, {,"thunderstorm" }, {,"windy" }, {,"hot" },
+            {, "light-wind" }, {,"clear" }, {,"partly-cloudy"};
+
+
+        }
     } 
 }
