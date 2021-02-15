01c8cec3cd0e3d0e46280db5a3be7d363ef615cc
Lloyd <UpstateLloyd@gmail.com>
2021-02-15 15:59:33 -0500

    Translating weather service terms to weather icons

--- Models/WeatherModel.cs
+++ Models/WeatherModel.cs
@@ -8,14 +8,6 @@ namespace WeatherAPI.Models
 {
     public class WeatherDefinitions
     {
-        public Dictionary<string, string> WeatherArray = new Dictionary<string, string>() 
-        {
-            { "Fair","sunny" } {"Mostly Cloudy","cloudy" }, {,"cloudy-gusts" }, {,"cloudy-windy" }, {,"fog" }, {,"hail" },
-            { "Haze","haze" }, {,"lightning" }, {,"rain" }, {,"rain-mix" }, {,"rain-wind" }, {,"showers" }, {,"sleet" }, {,"sleet-storm" },{,"snow" },
-            {,"snow-thunderstorm" }, {,"snow-wind" }, {,"sprinkle" }, {,"storm-showers" }, {,"sunny-overcast" }, {,"thunderstorm" }, {,"windy" }, {,"hot" },
-            {, "light-wind" }, {,"clear" }, {,"partly-cloudy"};
-
-
-        }
+      
     } 
 }
